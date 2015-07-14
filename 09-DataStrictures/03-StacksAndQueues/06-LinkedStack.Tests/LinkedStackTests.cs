ec869fb163a3f91ee8e182e33160ca441dd95372
dim4o <d_i_m@abv.bg>
2015-07-15 00:54:36 +0300

    Added: Unit tests

---
+++ 09-DataStrictures/03-StacksAndQueues/06-LinkedStack.Tests/LinkedStackTests.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using _05_LinkedStack;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _06_LinkedStackTests
+{
+    [TestClass]
+    public class LinkedStackTests
+    {
+        [TestMethod]
+        public void TestPushPopElement()
+        {
+            var stack = new LinkedStack<int>();
+            Assert.AreEqual(0, stack.Count);
+
+            stack.Push(1);
+            Assert.AreEqual(1, stack.Count);
+
+            stack.Pop();
+            Assert.AreEqual(0, stack.Count);
+        }
+
+        [TestMethod]
+        public void TestPush_1000_Elements()
+        {
+            var stack = new LinkedStack<int>();
+            Assert.AreEqual(0, stack.Count);
+
+            for (int i = 0; i < 1000; i++)
+            {
+                stack.Push(i);
+                Assert.AreEqual(i + 1, stack.Count);
+            }
+
+            for (int i = 0; i < 1000; i++)
+            {
+                stack.Pop();
+                Assert.AreEqual(1000 - i - 1, stack.Count);
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void TestPopFromEmptyStack()
+        {
+            var stack = new LinkedStack<int>();
+            stack.Pop();
+        }
+
+        [TestMethod]
+        public void TestStackToArrayAfterPushingNubers()
+        {
+            var stack = new LinkedStack<int>();
+            int[] arrayWithOrigins = { 3, 5, -2, 7 };
+
+            for (int i = 0; i < arrayWithOrigins.Length; i++)
+            {
+                stack.Push(arrayWithOrigins[i]);
+            }
+
+            int[] stackToArray = stack.ToArray();
+
+            for (int i = 0; i < arrayWithOrigins.Length; i++)
+            {
+                Assert.AreEqual(arrayWithOrigins[i], stackToArray[i]);
+            }
+        }
+
+        [TestMethod]
+        public void TestEmptyStackToArray()
+        {
+            var stack = new LinkedStack<DateTime>();
+            DateTime[] array = stack.ToArray();
+
+            Assert.AreEqual(0, array.Length);
+        }
+
+        [TestMethod]
+        public void TestPeakAfterPushPopElement()
+        {
+            var stack = new LinkedStack<int>();
+
+            stack.Push(10);
+            stack.Push(20);
+            Assert.AreEqual(20, stack.Peek());
+            stack.Pop();
+            Assert.AreEqual(10, stack.Peek());
+        }
+
+        [TestMethod]
+        public void TestCountAfterPushing()
+        {
+            var stack = new LinkedStack<int>();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            Assert.AreEqual(3, stack.Count);
+        }
+
+        [TestMethod]
+        public void TestCountAfterPoping()
+        {
+            var stack = new LinkedStack<int>();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Pop();
+            stack.Pop();
+            Assert.AreEqual(1, stack.Count);
+        }    
+    }
+}
---
+++ 09-DataStrictures/03-StacksAndQueues/08-LinkedQueue.Tests/LinkedQueueTests.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using _07_LinkedQueue;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _08_LinkedQueue.Tests
+{
+    [TestClass]
+    public class LinkedQueueTests
+    {
+
+        [TestMethod]
+        public void TestPushDequeueElement()
+        {
+            var queue = new LinkedQueue<int>();
+            Assert.AreEqual(0, queue.Count);
+
+            queue.Enqueue(1);
+            Assert.AreEqual(1, queue.Count);
+
+            queue.Dequeue();
+            Assert.AreEqual(0, queue.Count);
+        }
+
+        [TestMethod]
+        public void TestEnqueue_1000_Elements()
+        {
+            var queue = new LinkedQueue<int>();
+            Assert.AreEqual(0, queue.Count);
+
+            for (int i = 0; i < 1000; i++)
+            {
+                queue.Enqueue(i);
+                Assert.AreEqual(i + 1, queue.Count);
+            }
+
+            for (int i = 0; i < 1000; i++)
+            {
+                queue.Dequeue();
+                Assert.AreEqual(1000 - i - 1, queue.Count);
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void TestDequeueFromEmptyStack()
+        {
+            var queue = new LinkedQueue<int>();
+            queue.Dequeue();
+        }
+
+        [TestMethod]
+        public void TestQueueToArrayAfterEnqueueNubers()
+        {
+            var queue = new LinkedQueue<int>();
+            int[] arrayWithOrigins = { 3, 5, -2, 7 };
+
+            for (int i = 0; i < arrayWithOrigins.Length; i++)
+            {
+                queue.Enqueue(arrayWithOrigins[i]);
+            }
+
+            int[] stackToArray = queue.ToArray();
+
+            for (int i = 0; i < arrayWithOrigins.Length; i++)
+            {
+                Assert.AreEqual(arrayWithOrigins[i], stackToArray[i]);
+            }
+        }
+
+        [TestMethod]
+        public void TestEmptyQueueToArray()
+        {
+            var queue = new LinkedQueue<DateTime>();
+            DateTime[] array = queue.ToArray();
+
+            Assert.AreEqual(0, array.Length);
+        }
+
+        [TestMethod]
+        public void TestCountAfterEnqueue()
+        {
+            var queue = new LinkedQueue<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            Assert.AreEqual(3, queue.Count);
+        }
+
+        [TestMethod]
+        public void TestCountAfterDequeue()
+        {
+            var queue = new LinkedQueue<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Dequeue();
+            queue.Dequeue();
+            Assert.AreEqual(1, queue.Count);
+        }
+    }
+}
