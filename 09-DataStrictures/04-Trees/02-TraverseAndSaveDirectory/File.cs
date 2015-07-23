f860aaae1a8f32242df8b79a1c15e53ae2242677
dim4o <d_i_m@abv.bg>
2015-07-23 19:17:17 +0300

    Added: Trees homework

---
+++ 09-DataStrictures/04-Trees/02-TraverseAndSaveDirectory/File.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02_TraverseAndSaveDirectory
+{
+    public class File
+    {
+        public string Name { get; set; }
+
+        public long Size { get; set; }
+
+        public File(string name, long size)
+        {
+            this.Name = name;
+            this.Size = size;
+        }
+    }
+}
