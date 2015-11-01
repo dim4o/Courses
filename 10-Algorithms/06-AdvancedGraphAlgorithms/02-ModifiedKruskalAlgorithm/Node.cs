6eb36fef8ef4cc42872be9c4f42e916d9d72d700
dim4o <d_i_m@abv.bg>
2015-11-01 20:35:37 +0200

    added: Modified Kruskal

---
+++ 10-Algorithms/06-AdvancedGraphAlgorithms/02-ModifiedKruskalAlgorithm/Node.cs
@@ -0,0 +1,29 @@
+﻿namespace ModifiedKruskalAlgorithm
+{
+    using System.Collections.Generic;
+
+    class Node 
+    {
+        public int Value { get; set; }
+        public Node Parent { get; set; }
+        public List<Node> Children { get; set; }
+
+        public Node(int value)
+        {
+            this.Value = value;
+            this.Parent = this;
+            this.Children = new List<Node>();
+        }
+
+        public override bool Equals(object obj)
+        {
+            var item = obj as Node;
+
+            if (this.Value == item.Value)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
