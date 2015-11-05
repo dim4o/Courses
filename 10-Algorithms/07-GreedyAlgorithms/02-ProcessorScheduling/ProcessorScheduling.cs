b5dad8ef747853ab4429c90f6240c23d98c11f6f
dim4o <d_i_m@abv.bg>
2015-11-05 02:34:25 +0200

    Added: Greedy Algorithms Homework

---
+++ 10-Algorithms/07-GreedyAlgorithms/02-ProcessorScheduling/ProcessorScheduling.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class ProcessorScheduling
+{
+    static void Main()
+    {
+        Console.Write("Tasks: ");
+        int tasksCount = int.Parse(Console.ReadLine());
+        var tasks = new dynamic[tasksCount];
+        for (int i = 0; i < tasksCount; i++)
+        {
+            var taskParams = Console.ReadLine().Split('-').Select(int.Parse).ToList();
+            tasks[i] = new
+            {
+                TaskNumber = i + 1,
+                Value = taskParams[0],
+                Deadline = taskParams[1]
+            };
+        }
+
+        Array.Sort(tasks, (a, b) => b.Value.CompareTo(a.Value));
+        List<dynamic> optimalShedule = new List<dynamic>();
+        int totalValue = 0;
+
+        foreach (dynamic task in tasks)
+        {
+            if (CanAddTask(new List<dynamic>(optimalShedule), task))
+            {
+                optimalShedule.Add(task);
+                totalValue += task.Value;
+            }
+        }
+        var result = optimalShedule
+            .OrderBy(e => e.Deadline)
+            .ThenByDescending(e => e.Value);
+
+        Console.WriteLine("Optimal schedule: {0}", string.Join(" -> ", 
+            result.Select(e => e.TaskNumber)));
+        Console.WriteLine("Total value: {0}", totalValue);
+    }
+
+    private static bool CanAddTask(List<dynamic> shedule, dynamic taskToAdd)
+    {
+        shedule.Add(taskToAdd);
+        var currentShedule = shedule.OrderBy(e => e.Deadline).ToList();
+        return !currentShedule.Where((task, i) => task.Deadline < i + 1).Any();
+    }
+}
+
+//var tasks = new[]
+//{
+//    new { TaskNumber = 1, Value = 5, Deadline = 3 },
+//    new { TaskNumber = 2, Value = 6, Deadline = 4 },
+//    new { TaskNumber = 3, Value = 2, Deadline = 1 },
+//    new { TaskNumber = 4, Value = 3, Deadline = 4 },
+//    new { TaskNumber = 5, Value = 8, Deadline = 2 },
+//    new { TaskNumber = 6, Value = 4, Deadline = 3 },
+//};
---
+++ 10-Algorithms/07-GreedyAlgorithms/05-EgyptianFractions/EgyptianFractions.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace _05_EgyptianFractions
+{
+    class EgyptianFractions
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            var fractionParams = input.Split('/');
+            var numerator = int.Parse(fractionParams[0]);
+            var denominator = int.Parse(fractionParams[1]);
+
+            if (numerator >= denominator)
+            {
+                Console.WriteLine("Error (fraction is equal to or greater than 1)");
+                return;
+            }
+
+            Fraction number = new Fraction(numerator, denominator);
+            string result = input + " = ";
+
+            while (number.Numerator > 0)
+            {
+                int smallerDenominator = 2;
+
+                while (number.CompareTo(new Fraction(1, smallerDenominator)) < 0)
+                {
+                    smallerDenominator++;
+                }
+                var biggestFraction = new Fraction(1, smallerDenominator);
+                result += biggestFraction + " + ";
+
+                if (number.CompareTo(biggestFraction) == 0)
+                {
+                    break;
+                }
+                number = number - biggestFraction;
+            }
+
+            Console.WriteLine(result.Substring(0, result.Length - 3));
+        }
+    }
+
+    internal class Fraction : IComparable<Fraction>
+    {
+        public int Numerator { get; private set; }
+        private int Denominator { get; set; }
+
+        public Fraction(int numerator, int denominator)
+        {
+            this.Numerator = numerator;
+            this.Denominator = denominator;
+        }
+
+        public int CompareTo(Fraction other)
+        {
+            return (this.Numerator*other.Denominator)
+                .CompareTo(other.Numerator*this.Denominator);
+        }
+
+        public static Fraction operator -(Fraction first, Fraction second)
+        {
+            int nominator = first.Numerator * second.Denominator - second.Numerator * first.Denominator;
+            int denominator = first.Denominator*second.Denominator;
+            return new Fraction(nominator, denominator);
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0}/{1}", this.Numerator, this.Denominator);
+        }
+    }
+}
