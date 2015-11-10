91affc4b6270c603b244bc0d3387a1e9acb41c66
dim4o <d_i_m@abv.bg>
2015-11-10 10:57:28 +0200

    Dynamic programming: sum with limited amount of coins

---
+++ 10-Algorithms/04-DynamicProgramming/05-LimitedAmountOfCoinsSumIterative/LimitedAmountOfCoinsSumIterative.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05_LimitedAmountOfCoinsSumIterative
+{
+    class LimitedAmountOfCoinsSumIterative
+    {
+        static void Main()
+        {
+            Console.Write("S = ");
+            int targetSum = int.Parse(Console.ReadLine());
+            Console.Write("Coins = ");
+            string input = Console.ReadLine();
+            List<int> coins = input.Split(new char[] { '{', '}', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToList();
+            int[,] memo = new int[coins.Count, targetSum  +1];
+            coins.Sort();
+
+            if (coins[0] < targetSum)
+            {
+                memo[0, coins[0]] = 1;
+            }
+            for (int row = 1; row < memo.GetLength(0); row++)
+            {
+                for (int col = 1; col < memo.GetLength(1); col++)
+                {
+                    if (coins[row] > col)
+                    {
+                        memo[row, col] = memo[row - 1, col];
+                        continue;
+                    }
+                    if (coins[row] == col)
+                    {
+                        memo[row, col] = 1;
+                    }
+                    int prevNonEqualCoin = row;
+                    while (prevNonEqualCoin >= 0 && coins[row] == coins[prevNonEqualCoin])
+                    {
+                        prevNonEqualCoin--;
+                    }
+                    memo[row, col] += memo[row - 1, col - coins[row]] + memo[prevNonEqualCoin, col];
+                }
+            }
+
+            Console.WriteLine(memo[coins.Count-1, targetSum]);
+        }
+    }
+}
---
+++ 10-Algorithms/04-DynamicProgramming/05-SumWithLimitedAmountOfCoins.Tests/SumWithLimitedAmountOfCoinsTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Markup;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _05_SumWithLimitedAmountOfCoins.Tests
+{
+    [TestClass]
+    public class SumWithLimitedAmountOfCoinsTests
+    {
+        [TestMethod]
+        public void TestWith_sum_6_and_coins_1_2_2_3_3_4_6()
+        {
+            List<int> coins = new List<int>{1, 2, 2, 3, 3, 4, 6};
+            int targetSum = 6;
+            int[,] values = new int[targetSum + 1, targetSum + 1];
+
+            int count = SumWithLimitedAmountOfCoins.GetCount(coins, targetSum, coins.Count - 1, values);
+
+            Assert.AreEqual(4, count);
+        }
+
+        [TestMethod]
+        public void TestWith_sum_5_and_coins_1_2_2_5()
+        {
+            List<int> coins = new List<int> { 1, 2, 2, 5 };
+            int targetSum = 5;
+            int[,] values = new int[targetSum + 1, targetSum + 1];
+
+            int count = SumWithLimitedAmountOfCoins.GetCount(coins, targetSum, coins.Count - 1, values);
+
+            Assert.AreEqual(2, count);
+        }
+
+        [TestMethod]
+        public void TestWith_sum_13_and_coins_1_2_2_5_5_10()
+        {
+            List<int> coins = new List<int> { 1, 2, 2, 5, 5, 10 };
+            int targetSum = 13;
+            int[,] values = new int[targetSum + 1, targetSum + 1];
+
+            int count = SumWithLimitedAmountOfCoins.GetCount(coins, targetSum, coins.Count - 1, values);
+
+            Assert.AreEqual(2, count);
+        }
+
+        [TestMethod]
+        public void TestWith_sum_100_and_coins_50_20_20_20_20_20_10()
+        {
+            List<int> coins = new List<int> { 50, 20, 20, 20, 20, 20, 10 };
+            int targetSum = 100;
+            int[,] values = new int[targetSum + 1, targetSum + 1];
+
+            int count = SumWithLimitedAmountOfCoins.GetCount(coins, targetSum, coins.Count - 1, values);
+
+            Assert.AreEqual(2, count);
+        }
+    }
+}
